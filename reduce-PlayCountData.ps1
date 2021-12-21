744531c7e507c2349c21a2cc6523a8613beb96ae
ArgoTheNaut <36687789+ArgoTheNaut@users.noreply.github.com>
2021-12-20 20:48:40 -0500

    Create reduce-PlayCountData.ps1

---
+++ reduce-PlayCountData.ps1
@@ -0,0 +1,31 @@
+﻿param(
+[Parameter(Mandatory=$true)]$itemCSVfilePath,
+[Parameter(Mandatory=$true)]$itemStatsCSVfilePath,
+[Parameter(Mandatory=$true)]$exportCSVfilePath
+)
+
+
+
+
+
+cd ~\Desktop
+$itemData = Import-Csv $itemCSVfilePath
+$itemStatsData = Import-Csv $itemStatsCSVfilePath
+
+$minData = @()
+
+
+$itemData | foreach{
+    $nextSong = [pscustomobject]@{
+        GUID= "_"+$_.pid
+        artist = $_.artist
+        title = $_.title
+        plays = -1
+    }
+    Write-Host "Processing " $nextSong.artist " - " $nextSong.title
+   $nextSong.plays = ($itemStatsData | where {$nextSong.GUID -eq (“_” + $_.item_pid)}).play_count_user
+$minData += $nextSong
+}
+
+
+$minData | Export-Csv -Path $exportCSVfilePath -NoTypeInformation
